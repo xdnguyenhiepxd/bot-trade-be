0d1ca8834faa14e76e704987bc88f4dd34e2b881
onepercman <onepercman@gmail.com>
2024-05-05 22:24:46 +0700

    🚀 update swagger

--- src/main.ts
+++ src/main.ts
@@ -17,7 +17,7 @@ async function bootstrap() {
 		.addBearerAuth()
 		.build();
 	const document = SwaggerModule.createDocument(app, config);
-	SwaggerModule.setup("docs", app, document);
+	SwaggerModule.setup("", app, document);
 
 	await app.listen(environments.PORT || 4000);
 }
